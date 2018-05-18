2bbe7f57a9fb5e0631cfb30ba92f49ce33e0518b
sergio rodriguez <sergrodri92@gmail.com>
2018-05-17 19:50:13 -0500

    added color animations to components

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -6,17 +6,21 @@ import QuoteBoxStyle from './QuoteBoxStyle.css';
 import FaQuoteLeft from 'react-icons/lib/fa/quote-left';
 import FaQuoteRight from 'react-icons/lib/fa/quote-right';
 
-
+//const randomColors = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
 class QuoteBox extends React.Component{
+
+
 
   constructor(props){
     //Make sure to pass props to base constructor
     super(props);
     this.state = {
       quote: '',
-      author: ''
+      author: '',
+      color: '#64E6BA'
     }
   }
+
   componentDidMount() {
     fetch('https://talaikis.com/api/quotes/random/')
     .then(result => result.json())
@@ -25,12 +29,14 @@ class QuoteBox extends React.Component{
         quote: data.quote,
         author: data.author
       })
-      console.log(this.state.quote)
-      console.log(this.state.author)
-    });
+      // console.log(this.state.quote)
+      // console.log(this.state.author)
+    })
+
+
   }
 
-  getQuote = () => {
+  handleClick = () => {
     //fetch quote from api
     fetch('https://talaikis.com/api/quotes/random/')
     .then(result => result.json())
@@ -39,11 +45,14 @@ class QuoteBox extends React.Component{
         quote: data.quote,
         author: data.author
       })
-      console.log(data)
-      console.log(this.state.quote)
-      console.log(this.state.author)
-
+      // console.log(data)
+      // console.log(this.state.quote)
+      // console.log(this.state.author)
     });
+
+    //change color after Click
+    this.getRandomColor();
+    // console.log(this.state.color)
 
   }
 
@@ -53,26 +62,41 @@ class QuoteBox extends React.Component{
     console.log('twitter');
   }
 
+
+
+  getRandomColor = () => {
+    const randColors = ['#5d94af','#2aaa79','#2aaa79','#6e42ce']
+    let number = Math.floor(Math.random() * randColors.length);
+
+
+    this.setState({
+      color: randColors[number]
+    })
+    console.log(randColors[number]);
+  }
+
+
   render(){
     return(
     <React.Fragment>
-      <div className='Box'>
+      <div className='Box' style={{borderColor:this.state.color}}>
 
-        <p className='Quote'><FaQuoteLeft size={30} className='Color' /> {this.state.quote} <FaQuoteRight size={30} className='Color' /></p>
+        <p className='Quote'>
+          <FaQuoteLeft size={40} className='Color' style={{color: this.state.color, transitionDuration: '1s'}}/>
+          {this.state.quote}
+          <FaQuoteRight size={40} className='Color' style={{color: this.state.color, transitionDuration: '1s'}}/>
+        </p>
 
         <p className='Author'>-{this.state.author}</p>
 
-        <div className='Divider'></div>
+
 
         <div className='ButtonDiv'>
-          <TwitterButton click={this.shareTwitter}/>
-          <QuoteButton click={this.getQuote}/>
+          <TwitterButton color={this.state.color} click={this.shareTwitter}/>
+          <QuoteButton  color={this.state.color} click={this.handleClick} />
         </div>
 
       </div>
-
-
-
     </React.Fragment>
     );
   }
@@ -80,3 +104,4 @@ class QuoteBox extends React.Component{
 
 export default QuoteBox;
 //<button onClick={this.getQuote}>Display quote</button>
+  // <div className='Divider' style={{backgroundColor:this.state.color}}></div>
--- src/components/QuoteButton.js
+++ src/components/QuoteButton.js
@@ -5,7 +5,10 @@ class QuoteButton extends React.Component {
 
   render(){
     return(
-      <button className='ButtonStyle' onClick={this.props.click}>
+      <button className='ButtonStyle' onClick={this.props.click}
+         style={{
+           color: this.props.color,
+           borderColor: this.props.color}}>
         <FaArrowRight size={20}/>
       </button>
     );

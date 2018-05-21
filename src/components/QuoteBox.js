b626e6aeb4ff9224306d54ea8c2f1ed250a9ccf5
sergio rodriguez <sergrodri92@gmail.com>
2018-05-21 14:34:46 -0500

    background color in sync, added cssTransitionGroup package for fade animations.

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import QuoteButton from './QuoteButton.js';
 import TwitterButton from './TwitterButton.js';
 import QuoteBoxStyle from './QuoteBoxStyle.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import FaQuoteLeft from 'react-icons/lib/fa/quote-left';
 import FaQuoteRight from 'react-icons/lib/fa/quote-right';
@@ -22,6 +23,7 @@ class QuoteBox extends React.Component{
   }
 
   componentDidMount() {
+
     fetch('https://talaikis.com/api/quotes/random/')
     .then(result => result.json())
     .then(data => {
@@ -29,11 +31,10 @@ class QuoteBox extends React.Component{
         quote: data.quote,
         author: data.author
       })
+
       // console.log(this.state.quote)
       // console.log(this.state.author)
-    })
-
-
+    });
   }
 
   handleClick = () => {
@@ -45,6 +46,7 @@ class QuoteBox extends React.Component{
         quote: data.quote,
         author: data.author
       })
+      this.updateBodyColor();
       // console.log(data)
       // console.log(this.state.quote)
       // console.log(this.state.author)
@@ -52,8 +54,11 @@ class QuoteBox extends React.Component{
 
     //change color after Click
     this.getRandomColor();
-    // console.log(this.state.color)
 
+  }
+
+  updateBodyColor = () => {
+    document.body.style.backgroundColor = this.state.color;
   }
 
   shareTwitter = () => {
@@ -65,13 +70,14 @@ class QuoteBox extends React.Component{
 
 
   getRandomColor = () => {
-    const randColors = ['#5d94af','#2aaa79','#2aaa79','#6e42ce']
+    const randColors = ['#5d94af','#2aaa79','#6e42ce','#802D15','#D4826A','#D4A46A','#804F15','#6F90A2','#13B510','#6F4D8F','#9277AC','#69802E','#F01A83','#A55800','#FF971C','#FFD300','#FFD300']
     let number = Math.floor(Math.random() * randColors.length);
-
 
     this.setState({
       color: randColors[number]
     })
+    //update body background color when random color is selected
+
     console.log(randColors[number]);
   }
 
@@ -79,16 +85,25 @@ class QuoteBox extends React.Component{
   render(){
     return(
     <React.Fragment>
+
       <div className='Box' style={{borderColor:this.state.color}}>
+        <ReactCSSTransitionGroup
+          transitionName='fade'
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={.2}>
+            <p className='Quote' key={this.state.quote}>
+              <FaQuoteLeft size={40} className='Color' style={{color:this.state.color, transitionDuration: '1s'}}/>
+                {this.state.quote}
+              <FaQuoteRight size={40} className='Color' style={{color: this.state.color, transitionDuration: '1s'}}/>
+            </p>
+        </ReactCSSTransitionGroup>
 
-        <p className='Quote'>
-          <FaQuoteLeft size={40} className='Color' style={{color: this.state.color, transitionDuration: '1s'}}/>
-          {this.state.quote}
-          <FaQuoteRight size={40} className='Color' style={{color: this.state.color, transitionDuration: '1s'}}/>
-        </p>
-
-        <p className='Author'>-{this.state.author}</p>
-
+        <ReactCSSTransitionGroup
+          transitionName='fade'
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={.2}>
+            <p className='Author' key={this.state.author}>- {this.state.author}</p>
+        </ReactCSSTransitionGroup>
 
 
         <div className='ButtonDiv'>
@@ -96,7 +111,9 @@ class QuoteBox extends React.Component{
           <QuoteButton  color={this.state.color} click={this.handleClick} />
         </div>
 
+
       </div>
+
     </React.Fragment>
     );
   }
